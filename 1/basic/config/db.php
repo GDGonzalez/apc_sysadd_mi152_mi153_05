d04b8430d3b62fdab57da9ecd8320784f01a8810
Kevin Barrion <barrion.kevin@gmail.com>
2017-08-13 15:46:41 +0800

    rechange database name

--- 1/basic/config/db.php
+++ 1/basic/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=q',
+    'dsn' => 'mysql:host=localhost;dbname=ispector',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
