94d1cca8892b4e2180b9bf68822dc1386c458657
chantal15 <chantalsaldivar530@gmail.com>
2017-08-15 10:44:55 +0800

    Generated active record class for Workschedule table inside model folder

--- application/basic/controllers/RoomController.php
+++ application/basic/controllers/RoomController.php
@@ -8,6 +8,7 @@ use app\models\RoomSearch;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
 
 /**
  * RoomController implements the CRUD actions for Room model.
@@ -20,6 +21,17 @@ class RoomController extends Controller
     public function behaviors()
     {
         return [
+
+            'access'=>[
+            'class'=>AccessControl::classname(),
+            'only'=>['create','update'],
+            'rules'=>[
+                         [
+                            'allow'=>true,
+                            'roles'=>['@']
+                         ],
+                ]
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
